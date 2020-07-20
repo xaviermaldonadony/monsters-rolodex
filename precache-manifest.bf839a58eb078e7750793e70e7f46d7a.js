9669307d41e8b0cdf8bbeb9b8807165a7a784d80
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-20 04:40:39 -0400

    Updates

---
+++ precache-manifest.bf839a58eb078e7750793e70e7f46d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0605c255dbffd60e285486269cadf99",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "233a2577d0d73a936884",
+    "url": "/monsters-rolodex/static/css/main.459a0fef.chunk.css"
+  },
+  {
+    "revision": "eea39d6ccacff15cd72e",
+    "url": "/monsters-rolodex/static/js/2.ebb27119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.ebb27119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233a2577d0d73a936884",
+    "url": "/monsters-rolodex/static/js/main.43bae1f9.chunk.js"
+  },
+  {
+    "revision": "84fa8ae7cbe51b728f9b",
+    "url": "/monsters-rolodex/static/js/runtime-main.09db2c25.js"
+  }
+]);
